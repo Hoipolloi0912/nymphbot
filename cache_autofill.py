0344882b791ccd70464fbf8ef2b67b8312b95337
unknown <Kevin-yang2000@hotmail.com>
2026-05-07 10:43:21 +0900

    fixed issue where cache directory doesnt exist

--- cache_autofill.py
+++ cache_autofill.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 DB_URL = os.getenv("DB_URL")
-PATH = "cache/"
+PATH = "cache"
 
 def make_anime_json():
     with psycopg.connect(DB_URL) as conn:
@@ -18,7 +18,7 @@ def make_anime_json():
             rows = cur.fetchall()
             anime_dict = {id:[name_en.strip() if name_en else None,name_ja.strip() if name_ja else None] for id, name_en, name_ja in rows}
 
-    with open(f"{PATH}anime_map.json", "w", encoding="utf-8") as f:
+    with open(f"{PATH}/anime_map.json", "w", encoding="utf-8") as f:
         json.dump(anime_dict, f, ensure_ascii=False, indent=2)
     print("anime_map updated.")
 
@@ -32,7 +32,7 @@ def make_artist_json():
             rows = cur.fetchall()
             artist_dict = {id: name.strip() for id, name in rows}
 
-    with open(f"{PATH}artist_map.json", "w", encoding="utf-8") as f:
+    with open(f"{PATH}/artist_map.json", "w", encoding="utf-8") as f:
         json.dump(artist_dict, f, ensure_ascii=False, indent=2)
     print("artist_map updated.")
 
@@ -46,23 +46,24 @@ def make_song_json():
             rows = cur.fetchall()
             artist_dict = {id: [s_name.strip(),a_name.strip()] for id, s_name,a_name in rows}
 
-    with open(f"{PATH}song_map.json", "w", encoding="utf-8") as f:
+    with open(f"{PATH}/song_map.json", "w", encoding="utf-8") as f:
         json.dump(artist_dict, f, ensure_ascii=False, indent=2)
     print("song_map updated.")
 
 def get_artist_dict():
-    with open(f"{PATH}artist_map.json", "r", encoding="utf-8") as f:
+    with open(f"{PATH}/artist_map.json", "r", encoding="utf-8") as f:
         return json.load(f)
 
 def get_anime_dict():
-    with open(f"{PATH}anime_map.json","r", encoding="utf-8") as f:
+    with open(f"{PATH}/anime_map.json","r", encoding="utf-8") as f:
         return json.load(f)
     
 def get_song_dict():
-    with open(f"{PATH}song_map.json","r", encoding="utf-8") as f:
+    with open(f"{PATH}/song_map.json","r", encoding="utf-8") as f:
         return json.load(f)
 
 def update_autofill():
+    os.makedirs(PATH, exist_ok=True)
     make_anime_json()
     make_artist_json()
     make_song_json()
